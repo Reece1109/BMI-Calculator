a2b33a9892a886c65db8dbf61ae62b16acbb7186
Reece1109 <54347195+Reece1109@users.noreply.github.com>
2023-05-09 15:58:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const btn = document.getElementById("calculate");
-​
-function calculate () {
+
+btn.addEventListener("click", function calculate() {
   let height = document.querySelector("#height").value;
   let weight = document.querySelector("#weight").value;
-​
+
   if (height == "" || weight == "") {
     alert("Please fill out the input fields!");
     return;
   }
-​
+
   // BMI = weight in KG / (height in m * height in m)
-​
+
   height = height / 100;
-​
+
   let BMI = weight / (height * height);
-​
+
   BMI = BMI.toFixed(2);
-​
+
   document.querySelector("#result").innerHTML = BMI;
-​
+
   let status = "";
-​
+
   if (BMI < 18.5) {
     status = "Underweight";
   }
